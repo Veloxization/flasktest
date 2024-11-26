e12a6a6632c14d1f9d412a10bde82c15974904fa
Veloxization <github@velo.anonaddy.com>
2024-11-26 11:52:27 +0200

    Added port specification

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +9,7 @@ def home():
            <p>you awe wooking at a page OwO</p>""")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
